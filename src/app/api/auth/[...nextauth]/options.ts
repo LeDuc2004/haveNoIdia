75167f3766a7a91fd217320f710f7fd13f0a5957
ThanhDV <thanhdv2@rikkeisoft.com>
2024-01-30 12:51:21 +0700

    next auth

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,36 @@
+import type { NextAuthOptions } from 'next-auth'
+import GitHubProvider from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const options: NextAuthOptions = {
+    providers: [
+        GitHubProvider({
+            clientId: "24b396affdb5213be6ce",
+            clientSecret: "3462434af3255ed5b3f127611414ceb95df62090",
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username:",
+                    type: "text",
+                    placeholder: "your-cool-username"
+                },
+                password: {
+                    label: "Password:",
+                    type: "password",
+                    placeholder: "your-awesome-password"
+                }
+            },
+            async authorize(credentials) {
+                const user = { id: "42", name: "Dave", password: "nextauth" }
+
+                if (credentials?.username === user.name && credentials?.password === user.password) {
+                    return user
+                } else {
+                    return null
+                }
+            }
+        })
+    ],
+}
